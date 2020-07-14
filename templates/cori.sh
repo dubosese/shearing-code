b1ad3462b249c2a5f871befe3af51f2c1f2be429
Savannah <savannah.e.dubose@vanderbilt.edu>
2020-07-14 10:07:06 -0700

    Updates from Cori

--- templates/cori.sh
+++ templates/cori.sh
@@ -1,19 +1,35 @@
 {% extends "base_script.sh" %}
 {% block header %}
-#!/bin/bash
-#SBATCH --job-name=screening
+#!/bin/bash -l
+#SBATCH --job-name={{ name }}
 #SBATCH --nodes=1
-#SBATCH --ntasks=16
+#SBATCH --ntasks=64
 #SBATCH --time={{ walltime|format_timedelta }}
-#SBATCH -C haswell
+#SBATCH -C knl
 #SBATCH --error=error.err
-#SBATCH -q regular
+#SBATCH -q {{ queue }}
+#SBATCH --mail-type={{ mail }}
 
-echo working > test.txt
+module unload gromacs lammps
+module load gromacs/2020.1.knl lammps/2018.12.12-knl openmpi
+conda activate mosdef36
 
-module load python
-source activate mosdef36
-module load gromacs
-module load lammps
+{% endblock %}
+{% block body %}
+{% set cmd_suffix = cmd_suffix|default('') ~ (' &' if parallel else '') %}
+{% for operation in operations %}
+{% if operation.directives.nranks and not mpi_prefix %}
+{% set mpi_prefix = "" %}
+{% endif %}
 
+{% if operation.directives.omp_num_threads %}
+export OMP_NUM_THREADS={{ operation.directives.omp_num_threads }}
+{% endif %}
+{{ mpi_prefix }}{{ cmd_prefix }}{{ operation.cmd }}{{ cmd_suffix }}
+{% endfor %}
+{% endblock %}
+{% block footer %}
+{% if parallel %}
+wait
+{% endif %}
 {% endblock %}
